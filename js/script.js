354e571ef7402ac25c3fc51bd25babb05e5c46f9
Dogukan Basaran <dogukanbasarann@hotmail.com>
2024-11-30 21:08:38 +0300

    9

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-const header = document.querySelector("header");
-const footer = document.querySelector("footer");
-const body = document.querySelector("body");
-const text = document.querySelector("#home h1");
-const strongText = document.querySelector("#home h1 strong");
-const contactBtn = document.querySelector("#home button");
-
-window.addEventListener("load", () => {
-    if(window.location.toString().includes("index")){
-        header.style.display = "none";
-        footer.style.display = "none";
-        body.style.background = `var(--bgTerminal)`;
-        strongText.style.color = "#000";
-    }
-});
-
 const navList = document.querySelector("nav ul");
 const mobileMenu = document.querySelector("header #btn-menu");
 const searchBtn = document.querySelector("nav ul .search-box #btn-search");
@@ -36,16 +20,3 @@ mobileMenu.addEventListener("click", () => {
 searchBtn.addEventListener("click", () => {
     searchInput.focus();
 });
-
-const userInput = document.querySelector("#input-user");
-
-userInput.addEventListener("keypress", (e) => {
-    if(userInput.value === "hi" && e.key === "Enter"){
-        userInput.style.display = "none";
-        header.style.display = "block";
-        footer.style.display = "block";
-        body.style.background = `var(--bgColor)`;
-        text.style.color = `var(--txtColor)`;
-        contactBtn.style.display = "block";
-    }
-})
